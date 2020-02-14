9ce076406e490feaac99aab2d8429ce6147f61d8
Noriuki <lanezupiroli@gmail.com>
2020-02-13 22:09:19 -0300

    media queries e page404

--- static/js/site.js
+++ static/js/site.js
@@ -1,5 +1,22 @@
-jQuery( document ).ready( function( $ ) {
+function responsiveMenu() {
+  var x = document.getElementById("menu");
+  if (x.className === "container") {
+    x.className += " active";
+  } else {
+    x.className = "container";
+  }
+}
 
-  // Your JavaScript goes here
+window.onscroll = function() {fixeNav()};
 
-});
+var header = document.getElementById("header");
+
+var sticky = header.offsetTop;
+
+function fixeNav() {
+   if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+   } else {
+      header.classList.remove("sticky");
+   }
+}
